91cca987a1cb85933ace2ae4e893159c62f6b4dd
chrisonoo <chris@onoo.no>
2024-02-23 10:30:34 +0100

    Refactor class definitions and remove unused using directives
    Removed the explicit ContentPage and MauiWinUIApplication inheritance from various page classes in the FitnessAI.Maui application. This simplifies the class definitions. Also removed unnecessary "using" directives from several class files, making the codebase cleaner and easier to read.

--- FitnessAI.Maui/Pages/ProfilePage.xaml.cs
+++ FitnessAI.Maui/Pages/ProfilePage.xaml.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace FitnessAI.Maui.Pages;
 
-public partial class ProfilePage : ContentPage
+public partial class ProfilePage
 {
     public ProfilePage()
     {
--- FitnessAI.Maui/Pages/TicketPage.xaml.cs
+++ FitnessAI.Maui/Pages/TicketPage.xaml.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace FitnessAI.Maui.Pages;
 
-public partial class TicketPage : ContentPage
+public partial class TicketPage
 {
     public TicketPage()
     {
