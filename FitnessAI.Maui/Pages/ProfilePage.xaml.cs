6a21e0c287543a2c5ef59844431a2e57190e8c16
chrisonoo <chris@onoo.no>
2024-02-23 12:15:54 +0100

    Add UI labels and implement logout function
    This commit adds labels to the 'TicketPage' and 'MainPage' and updates the 'ProfilePage' with user profile display and logout function. It enhances user navigation and interaction by clarifying pages and providing logout feature to ensure user security.

--- FitnessAI.Maui/Pages/ProfilePage.xaml.cs
+++ FitnessAI.Maui/Pages/ProfilePage.xaml.cs
@@ -5,5 +5,13 @@ public partial class ProfilePage
     public ProfilePage()
     {
         InitializeComponent();
+
+        LblUsername.Text = Preferences.Get("user_name", string.Empty);
+    }
+
+    private void BtnLogout_OnClicked(object? sender, EventArgs e)
+    {
+        Preferences.Set("access_token", string.Empty);
+        Application.Current!.MainPage = new NavigationPage(new LoginPage());
     }
 }
